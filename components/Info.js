2512050ca92117af4120676dd1a8cd102e3d861d
nayul34 <dagg762@gmail.com>
2023-04-13 23:19:25 +0900

    add map

--- components/Info.js
+++ components/Info.js
@@ -8,7 +8,15 @@ const Info = () => {
         <p>등록번호 :</p>
         <p>등록주소 :</p>
       </SInfo>
-      <SMap />
+      <SMap>
+        <iframe
+          className="map"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13046.139639185318!2d128.1333656!3d35.1682182!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356efddfc971f8f3%3A0x45585e23ac489df1!2z67aA6rCV67aA64-Z7IKw!5e0!3m2!1sko!2skr!4v1681395019395!5m2!1sko!2skr"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </SMap>
     </SWrapper>
   );
 };
@@ -31,5 +39,10 @@ const SMap = styled.main`
   height: 70%;
   background-color: white;
   margin-left: 10px;
+  .map {
+    width: full;
+    height: 300px;
+    border: none;
+  }
 `;
 export default Info;
