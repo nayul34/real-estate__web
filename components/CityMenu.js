3139ff41e52bba5cf625ac3f44dec227428b459d
nayul34 <dagg762@gmail.com>
2023-05-01 23:52:30 +0900

    카테고리 모달 창 수정

--- components/CityMenu.js
+++ components/CityMenu.js
@@ -1,14 +1,88 @@
 import styled from "styled-components";
 
-const CityMenu = ({ setCityOpen }) => {
+const CityMenu = ({ setCityOpen, data }) => {
   const OnCityClose = () => {
     setCityOpen(false);
   };
-  return <SWrapper onClick={OnCityClose}>dfdfs</SWrapper>;
+
+  return (
+    <>
+      <SModalBack>
+        <SWrapper>
+          <div className="box">
+            <div className="top">
+              <p>진주 혁신도시</p>
+              <div className="close" onClick={OnCityClose}>
+                x
+              </div>
+            </div>
+
+            <p>(클릭시 관련 블로그로 이동합니다.)</p>
+            <SCategory>
+              {data.map((data) => (
+                <div className="category" key={data.id}>
+                  {data.title && <button>{data.title}</button>}
+                  <div className="choice">
+                    {data.category1 && <button>{data.category1}</button>}
+                    {data.category2 && <button>{data.category2}</button>}
+                    {data.category3 && <button>{data.category3}</button>}
+                    {data.category4 && <button>{data.category4}</button>}
+                  </div>
+                </div>
+              ))}
+            </SCategory>
+          </div>
+        </SWrapper>
+      </SModalBack>
+    </>
+  );
 };
+export const SModalBack = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  top: 0%;
+  left: 0%;
+`;
 const SWrapper = styled.div`
   width: 100%;
   height: 100%;
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+  top: 0%;
+  .box {
+    width: 50%;
+    height: 50%;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .top {
+    display: flex;
+    .close {
+      font-size: 2rem;
+      width: 50%;
+      text-align: right;
+    }
+  }
+`;
+const SCategory = styled.div`
+  width: 80%;
+  height: 80%;
+  /* background-color: skyblue;xw */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  .category {
+    display: flex;
+    .choice {
+      display: flex;
+    }
+  }
 `;
 export default CityMenu;
--- components/StationMenu.js
+++ components/StationMenu.js
@@ -1,14 +1,80 @@
 import styled from "styled-components";
 
-const StationMenu = ({ setStationOpen }) => {
+const StationnMenu = ({ setStationOpen, data }) => {
   const OnStationClose = () => {
     setStationOpen(false);
   };
-  return <SWrapper onClick={OnStationClose}>hihi</SWrapper>;
+  console.log(data);
+  return (
+    <>
+      <SModalBack>
+        <SWrapper>
+          <div className="box">
+            <div className="top">
+              <p>{data[0].title}</p>
+              <div className="close" onClick={OnStationClose}>
+                x
+              </div>
+            </div>
+
+            <p>(클릭시 관련 블로그로 이동합니다.)</p>
+            <SCategory>
+              {data.map((data) => (
+                <div className="category" key={data.id}>
+                  <p>{data.category1}</p>
+                  <p>{data.category2}</p>
+                  <p>{data.category3}</p>
+                  <p>{data.category4}</p>
+                </div>
+              ))}
+            </SCategory>
+          </div>
+        </SWrapper>
+      </SModalBack>
+    </>
+  );
 };
+export const SModalBack = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  top: 0%;
+  left: 0%;
+`;
 const SWrapper = styled.div`
   width: 100%;
   height: 100%;
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .top {
+    display: flex;
+  }
+  .close {
+    font-size: 2rem;
+    width: 50%;
+    text-align: right;
+  }
+  .box {
+    width: 50%;
+    height: 50%;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  z-index: 999;
+  top: 0%;
 `;
-export default StationMenu;
+const SCategory = styled.div`
+  width: 80%;
+  height: 80%;
+  background-color: skyblue;
+  display: flex;
+  justify-content: space-between;
+  .category {
+  }
+`;
+export default StationnMenu;
