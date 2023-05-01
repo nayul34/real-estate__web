8be115be4af4c96070bced959dc0418b3d5aa380
nayul34 <dagg762@gmail.com>
2023-05-01 23:51:39 +0900

    footer, header css 수정

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
 };
 const SWrapper = styled.div`
   width: 100%;
-  height: 20vh;
-  background-color: ivory;
+  height: 15vh;
+  background-color: gainsboro;
   margin-top: 20px;
 `;
 export default Footer;
--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
-const Header = () => {
+const Header = ({ data }) => {
   return (
     <SWrapper>
-      <p className="title">부강부동산</p>
+      <p className="title">{data[0].title}</p>
     </SWrapper>
   );
 };
+
 const SWrapper = styled.div`
-  height: 20vh;
-  background-color: aliceblue;
+  height: 15vh;
+  width: 100%;
+  border-bottom: 2px solid gray;
+  color: gray;
   display: flex;
   justify-content: center;
   align-items: center;
